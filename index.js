013f103e5c2149e42314c88508693d703f6b09ae
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-07-24 10:22:56 +0200

    mejora de funciones y cambios finales

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ moreBtn$.classList.add('box--btn-hide')
 let countPage = 0;
 
 
-
 const callApi =  () => {
     fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${countPage}&limit=20`)
         .then(res => res.json())
         .then(pokemons => {printPokemonsCards(pokemons.results)})    
 }
 callApi();
-
 
 const printPokemonsCards = pokemons => {
     for (const pokemon of pokemons) {
@@ -29,13 +27,16 @@ const printOnePokemon = (pokemon) => {
     const div$$ = document.createElement('div');
     div$$.classList.add('b-card-container__card');
     div$$.classList.add('box');
-    div$$.innerHTML = `<h2>${pokemon.name}</h2>`;
+    const h2$$ = document.createElement('h2');
+    h2$$.innerHTML = pokemon.name;
+    div$$.appendChild(h2$$)
     div$$.setAttribute('id', pokemon.name);
     callApiImgs(div$$)
     
     cardsContainer$$.appendChild(div$$);
     cardsContainer$$.appendChild(moreBtn$);
-    div$$.addEventListener('click', () => showDesc(div$$))
+
+    h2$$.addEventListener('click', () => showDesc(div$$))
 }
 
 const callApiImgs = (div) => {
@@ -51,64 +52,67 @@ const printPokemonsImg = (pokemon, div) => {
 }
 
 const showDesc = (div$$) => {
-    const divContainer$$ = document.createElement('div');
-    const ul$$ = document.createElement('ul');
-    const firstLi$$ = document.createElement('li');
-    const secLi$$ = document.createElement('li');
-    firstLi$$.textContent = 'Description';
-    secLi$$.textContent = 'Stats';
-
-
-
+    printInfo(div$$)
+    const divContainer$$ = document.querySelector('.b-description__container');
+    const firstLi$$ = document.querySelector('.b-description__container--desc');
+    firstLi$$.style.borderBottom = '2px solid #EDEDED';
+    const secLi$$ = document.querySelector('.b-description__container--stats')
     secLi$$.addEventListener('click', (e) =>{ 
         e.stopPropagation(); 
-        showStats(divContainer$$, div$$)});
+        showStats(div$$, divContainer$$)});
 
-
-
-    ul$$.appendChild(firstLi$$);
-    ul$$.appendChild(secLi$$);
-    divContainer$$.appendChild(ul$$);
-    divContainer$$.classList.add('b-description__container')
-    
-    div$$.appendChild(divContainer$$)
-    
     callApiBio(div$$, divContainer$$);
 }
 
 
-
-
-
-
-const showStats = (divContainer, div) => {
-    divContainer.innerHTML = ''
-    callApiStats(div, divContainer);
-}
-const printPokemonsStats = (pokemons, divContainer) => {
+const printInfo = (div$$) => {
     const divContainer$$ = document.createElement('div');
     const ul$$ = document.createElement('ul');
     const firstLi$$ = document.createElement('li');
     const secLi$$ = document.createElement('li');
+    firstLi$$.classList.add('b-description__container--desc')
+    secLi$$.classList.add('b-description__container--stats')
     firstLi$$.textContent = 'Description';
     secLi$$.textContent = 'Stats';
     ul$$.appendChild(firstLi$$);
     ul$$.appendChild(secLi$$);
     divContainer$$.appendChild(ul$$);
-    divContainer.appendChild(divContainer$$)
+    divContainer$$.classList.add('b-description__container')
+    div$$.appendChild(divContainer$$)
+}
+
+
+const showStats = (div, divContainer$$) => {
+    divContainer$$.remove();
+    callApiStats(div);
+}
+const printPokemonsStats = (pokemons, div) => {
+    printInfo(div);
+
+    const divContainer$$ = document.querySelector('.b-description__container');
+    const firstLi$$ = document.querySelector('.b-description__container--desc');
+    const secLi$$ = document.querySelector('.b-description__container--stats');
+    secLi$$.style.borderBottom = '2px solid #EDEDED';
+
+    firstLi$$.addEventListener('click', (e) =>{ 
+        e.stopPropagation(); 
+        divContainer$$.remove();
+        showDesc(div)});
 
     for (const stat of pokemons.stats) {
         const globalDiv$$ = document.createElement('div');
+        globalDiv$$.classList.add('b-description__container--progress')
         globalDiv$$.innerHTML = `
             <div class="progress-container">
                 <div class=""progress-bar style="
-                background-color: white;
+                background-color: #DA0138;
+                max-width: 100%;
                 width: ${stat.base_stat}%;
                 height: 20px;
                 border-radius: 5px;
             "></div>
             </div>
-            <div class="progress-text"><p>${stat.stat.name}: ${stat.base_stat}</p></div>
+            <div class="progress-text"><p>${stat.stat.name}: ${stat.base_stat}%</p></div>
         `    
         divContainer$$.appendChild(globalDiv$$)
     }
@@ -122,20 +126,18 @@ const printPokemonsStats = (pokemons, divContainer) => {
     divHide$$.addEventListener('click', (e) =>{ 
         e.stopPropagation(); 
         hide(divContainer$$)});
-
 }
 
-const callApiStats = (div, divContainer) => {
+const callApiStats = (div) => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${div.id}/`)
         .then(res => res.json())
-        .then(pokemonsStats => {printPokemonsStats(pokemonsStats, divContainer)})
+        .then(pokemonsStats => {printPokemonsStats(pokemonsStats, div)})
 }
-
-
 
 
 const printPokemonsBio = (pokemons, divContainer$$) => {
     const globalDiv$$ = document.createElement('div');
+    globalDiv$$.classList.add('b-description__container--global');
     const divBiodata$$ = document.createElement('div');
     divBiodata$$.classList.add('b-description__container--biodata');
     divBiodata$$.innerHTML = `
@@ -165,7 +167,7 @@ const printPokemonsBio = (pokemons, divContainer$$) => {
     const divHide$$ = document.createElement('button');
     divHide$$.classList.add('box');
     divHide$$.classList.add('box--btn-hide');
-    divHide$$.textContent = 'Hide description';    
+    divHide$$.textContent = 'Hide description';   
 
     globalDiv$$.appendChild(divBiodata$$);    
     divTypes$$.appendChild(divTypesBtn$$);
@@ -176,6 +178,7 @@ const printPokemonsBio = (pokemons, divContainer$$) => {
         e.stopPropagation(); 
         hide(divContainer$$)});
 }
+
 
 const callApiBio = (div, divContainer$$) => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${div.id}/`)
@@ -199,7 +202,6 @@ const search = () => {
     fetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154`)
         .then(res => res.json())
         .then(pokemons => filterPokemon(pokemons))
-
 
     const backBtn$$ = document.createElement('button');
     backBtn$$.innerHTML = '<img src="assets/arrow.svg">';
